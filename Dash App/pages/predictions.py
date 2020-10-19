110e949629d90ff39793bb9e2bd8e6e69a3f011f
Lester Gomez <68140323+Lolo-17@users.noreply.github.com>
2020-10-19 18:57:37 -0400

    dash app prep

--- Dash App/pages/predictions.py
+++ Dash App/pages/predictions.py
@@ -4,6 +4,8 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from joblib import load
+import pandas as pd
 
 # Imports from this application
 from app import app
--- Dash App/run.py
+++ Dash App/run.py
@@ -11,7 +11,7 @@ from pages import index, predictions, insights, process
 
 # Navbar docs: https://dash-bootstrap-components.opensource.faculty.ai/l/components/navbar
 navbar = dbc.NavbarSimple(
-    brand='YOUR APP NAME',
+    brand='Kickstarter Success Prediction',
     brand_href='/', 
     children=[
         dbc.NavItem(dcc.Link('Predictions', href='/predictions', className='nav-link')), 
@@ -35,11 +35,10 @@ footer = dbc.Container(
         dbc.Col(
             html.P(
                 [
-                    html.Span('Your Name', className='mr-2'), 
-                    html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:<you>@<provider>.com'), 
-                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/<you>/<repo>'), 
-                    html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/<you>/'), 
-                    html.A(html.I(className='fab fa-twitter-square mr-1'), href='https://twitter.com/<you>'), 
+                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/Kickstarter-Success-Build/front-end'),
+                    html.Span('Ahmed Hadri -', className='mr-2'), 
+                    html.Span('Lester Gomez -', className='mr-2'), 
+                    html.Span('Steven Lee', className='mr-2'),
                 ], 
                 className='lead'
             )
