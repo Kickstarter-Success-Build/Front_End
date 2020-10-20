e7234262f202bd936b7dfda6234303075f45d2e0
Lester Gomez <68140323+Lolo-17@users.noreply.github.com>
2020-10-20 00:56:53 -0400

    .

--- Dash App/pages/index.py
+++ Dash App/pages/index.py
@@ -15,29 +15,19 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
-            ## Your Value Proposition
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
+            ## <Your Value Proposition>
 
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Get Prediction', color='primary'), href='/predictions')
     ],
     md=4,
 )
 
-gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=60)
-
 column2 = dbc.Col(
     [
-        dcc.Graph(figure=fig),
+    
     ]
 )
 
--- Dash App/pages/predictions.py
+++ Dash App/pages/predictions.py
@@ -3,34 +3,86 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from joblib import load
 import pandas as pd
 
 # Imports from this application
 from app import app
 
+model = load('assets/regression.joblib')
+
+@app.callback(
+    Output('prediction-content', 'children'),
+            [
+                Input('blurb', 'value'),
+                Input('state', 'value'),
+                Input('binary_state', 'value'),
+            ]
+)
+
+def predict(blurb, state, binary_state):
+    df = pd.DataFrame(
+        columns=['blurb', 'state', 'binary_state'],
+        data=[[blurb, state, binary_state]]
+    )
+
+    y_pred = model.predict(df)[0]
+    return f'Prediction ---> {y_pred}'
+
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 column1 = dbc.Col(
     [
+        # """image"""
+        # html.Img(src='assets/car crash.jpg', className='img-fluid'),
+
         dcc.Markdown(
             """
-        
-            ## Predictions
 
-            Your instructions: How to use your app to get new predictions.
+            ## **Make Predictions**
 
             """
         ),
+
+        dcc.Markdown(
+            """
+
+            Below is the prediction:
+
+            """
+        )
+        # ,
+        # html.H2('Predicted Kickstarter Success', className='mb-5'),
+        # html.Div(id='prediction-content', className='lead')
     ],
     md=4,
 )
 
 column2 = dbc.Col(
     [
-
+        dcc.Markdown('#### **Enter blurb below**'),
+        html.Div([
+            dcc.Textarea(
+            id='blurb',
+            placeholder='Enter a description...',
+            value='Ex: Sneak in, find treasures, avoid cats and collect the loot before time runs out!',
+            style={'width': '75%', 'height': 125},
+            className='mb-5',
+        )
+        html.Button('Submit', id='blurb-button', n_clicks=0),
+        html.Div(id='prediction-content', className='lead')
+        ])
     ]
 )
+
+@app.callback(
+    Output('prediction-content', 'children'),
+    [Input('blurb-button', 'n_clicks')],
+    [State('blurb', 'value')]
+)
+def update_output(n_clicks, value):
+    if n_clicks > 0:
+        return 'You have entered: \n{}'.format(value)
 
 layout = dbc.Row([column1, column2])
